66c224b7fbf9ccd24ef2483be035b7d31e8c6210
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-12-18 16:25:14 +0530

    second largest element

--- Arrays/ArrayBasic/LargestElement.js
+++ Arrays/ArrayBasic/LargestElement.js
@@ -1,11 +1,31 @@
-function largestElement(arr){
-   let largestElement = 0;
-   for(let i = 0;i<arr.length;i++){
-    if(arr[i]>largestElement){
-        largestElement = arr[i];
-    }
-   }
-   return largestElement;
-}
+// largest element
 
-console.log(largestElement([1,22,343,45,656,45,454]))
+// function largestElement(arr){
+//    let largestElement = 0;
+//    for(let i = 0;i<arr.length;i++){
+//     if(arr[i]>largestElement){
+//         largestElement = arr[i];
+//     }
+//    }
+//    return largestElement;
+// }
+// console.log(largestElement([1,22,343,45,656,45,454]))
+
+// second largest element
+
+function secondLargestElement(arr){
+    let largestElement = 0;
+    let secondLargestElement = 0;
+    for(let i = 0; i<arr.length;i++){
+     
+        if(arr[i]>largestElement){
+            largestElement=arr[i];
+        }
+        if(arr[i]>secondLargestElement && arr[i]<largestElement){
+            secondLargestElement=arr[i]
+        }
+    }
+    return secondLargestElement
+
+}
+console.log(secondLargestElement([1,22,343,45,656,45,454]))
